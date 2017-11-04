5e3b8ac4ccee0bf62395f3c422a3630596024596
kburd <kburd@udel.edu>
2017-11-03 23:08:00 -0400

    base buttons

--- app/src/main/java/com/example/kaleb/wavealpha/MainMenu.java
+++ app/src/main/java/com/example/kaleb/wavealpha/MainMenu.java
@@ -6,6 +6,9 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -16,5 +19,27 @@ public class MainMenu extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Button b =(Button) findViewById(R.id.stores);
+        int width = getResources().getDisplayMetrics().widthPixels/4;
+        int hei=getResources().getDisplayMetrics().heightPixels/4;
+        b.setLayoutParams(new LinearLayout.LayoutParams(width,hei));
+
+        b =(Button) findViewById(R.id.voucher);
+        width = getResources().getDisplayMetrics().widthPixels/4;
+        hei=getResources().getDisplayMetrics().heightPixels/4;
+        b.setLayoutParams(new LinearLayout.LayoutParams(width,hei));
+
+        b =(Button) findViewById(R.id.calendar);
+        width = getResources().getDisplayMetrics().widthPixels/4;
+        hei=getResources().getDisplayMetrics().heightPixels/4;
+        b.setLayoutParams(new LinearLayout.LayoutParams(width,hei));
+
+        b =(Button) findViewById(R.id.settings);
+        width = getResources().getDisplayMetrics().widthPixels/4;
+        hei=getResources().getDisplayMetrics().heightPixels/4;
+        b.setLayoutParams(new LinearLayout.LayoutParams(width,hei));
+
     }
 }
+
+
