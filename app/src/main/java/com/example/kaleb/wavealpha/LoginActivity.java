4fa232002b323853976dccdf9c8844f0f596fd43
kburd <kburd@udel.edu>
2017-11-03 20:53:26 -0400

    Linked two files

--- app/src/main/java/com/example/kaleb/wavealpha/LoginActivity.java
+++ app/src/main/java/com/example/kaleb/wavealpha/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.kaleb.wavealpha;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -324,6 +325,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
 
             // TODO: register the new account here.
+            Intent i = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(i);
+
+
+
             return true;
         }
 
